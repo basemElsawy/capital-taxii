b9f0d2234b3b22db2ba89e1039c1db89a6ea09f5
Basem mohamed abuzaid <besoelpop2@gmail.com>
2024-08-26 19:12:51 +0300

    fixing the environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,5 @@
 export const environment = {
   base_url: 'http://10.4.30.8:1222/api/',
-<<<<<<< HEAD
   image: 'http://10.4.30.8:1222/',
-=======
-  image: 'http://10.4.30.8:1222',
->>>>>>> a6362e86b6f7f8d58b06cd17fe53724f7cff6522
   production: false,
 };
