a120e376b0194ed8787733c80cb3bb6bb5229ed1
Ali8970 <aliabdelftah104@gmail.com>
2024-09-18 18:14:37 +0300

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  base_url: 'http://10.4.30.8:1222/api/',
-  image: 'http://10.4.30.8:1222',
+  base_url: 'http://10.1.4.204:1222/api/',
+  image: 'http://10.1.4.204:1222',
   production: true,
 };
